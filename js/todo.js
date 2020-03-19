5ed76d2d4a90bb243db153a72268e4281460ca45
Yoann <yoannb641@hotmail.com>
2020-03-19 20:07:28 +0100

    Initialisation du projet

---
+++ js/todo.js
@@ -0,0 +1,13 @@
+var todoList = {
+
+    todoList_items: [
+        {id: 0, value: "Ceci est ma première tache"},
+        {id: 1, value: "Ceci est ma seconde tache"},
+        {id: 2, value: "Ceci est ma troisième tache"},
+    ],
+
+    test: document.querySelector('ul')
+}
+
+
+var li = document.createElement(<li>Cheese</li>);
