9d32b372f1dd8b6d0ff3f2336497fa2959dd9773
Yoann <yoannb641@hotmail.com>
2020-03-21 14:47:15 +0100

    Travail sur la partie Javascript

--- js/todo.js
+++ js/todo.js
@@ -1,30 +1,47 @@
 var todoList = {
 
-    todoList: [
-        {id: 0, value: "Ceci est ma première tache", done:false},
-        {id: 1, value: "Ceci est ma seconde tache", done:true},
-        {id: 2, value: "Ceci est ma troisième tache", done:false},
+    taskList: [
+        {text: "Ceci est ma première tache", done:false},
+        {text: "Ceci est ma seconde tache", done:true},
+        {text: "Ceci est ma troisième tache", done:false},
     ],
 
     showTodos: function () {
 
         var ul = document.querySelector('ul');
+        var id = 0;
         ul.innerHTML = '';
 
-        this.todoList.forEach(todo => {
+        this.taskList.forEach(todo => {
             let task = document.createElement('li');
             let check = document.createElement('input');
             task.appendChild(check).setAttribute("type", "checkbox");
             check.checked = todo.done;
-            task.id = todo.id;
-            task.insertAdjacentText('beforeend', todo.value);
+            task.id = id;
+            task.insertAdjacentText('beforeend', todo.text);
             ul.appendChild(task);
+            id += 1;
         });
 
-    }
+    },
 
+    addTodo: function(text) {
+        let task = {text: text, done: false};
+        this.taskList.push(task);
+        this.showTodos();
+    },
+
+    changeTodo: function(position, newText) {
+        this.taskList[position].text = newText;
+        this.showTodos();
+    },
+
+    delTodo: function(id) {
+        this.taskList.splice(id, 1);
+        this.showTodos();
+    }
 
 
 }
 
-document.createElement('input')
+todoList.showTodos();
