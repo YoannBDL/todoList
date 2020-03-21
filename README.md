6dcecf373585e7b2797b5d0dfb0a9837e398f02f
Yoann <yoannb641@hotmail.com>
2020-03-21 10:42:47 +0100

    Travail sur code Js

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # todoList-project
 Mon premier projet Javascript, une todoList pour être original
 
-Liste des améliorations à prévoir :
+Liste des améliorations à apporter :
 
-- Afficher la liste des taches grace à une fonction Javacript
+- Afficher la liste des taches grace à une fonction Javacript // OK
+- Ajouter des tâches via boite de dialogue/bouton/entrée
--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,30 @@
 var todoList = {
 
-    todoList_items: [
-        {id: 0, value: "Ceci est ma première tache"},
-        {id: 1, value: "Ceci est ma seconde tache"},
-        {id: 2, value: "Ceci est ma troisième tache"},
+    todoList: [
+        {id: 0, value: "Ceci est ma première tache", done:false},
+        {id: 1, value: "Ceci est ma seconde tache", done:true},
+        {id: 2, value: "Ceci est ma troisième tache", done:false},
     ],
 
-    test: document.querySelector('ul')
+    showTodos: function () {
+
+        var ul = document.querySelector('ul');
+        ul.innerHTML = '';
+
+        this.todoList.forEach(todo => {
+            let task = document.createElement('li');
+            let check = document.createElement('input');
+            task.appendChild(check).setAttribute("type", "checkbox");
+            check.checked = todo.done;
+            task.id = todo.id;
+            task.insertAdjacentText('beforeend', todo.value);
+            ul.appendChild(task);
+        });
+
+    }
+
+
+
 }
 
-
-var li = document.createElement(li);
+document.createElement('input')
