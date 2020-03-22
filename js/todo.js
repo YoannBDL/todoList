e7693c4073a9596d3702668424a902726fda9d63
Yoann <yoannb641@hotmail.com>
2020-03-22 15:10:16 +0100

    Css...

--- js/todo.js
+++ js/todo.js
@@ -44,25 +44,21 @@ var view = {
         for (let i=0; i<todoList.taskList.length; i++){
             let task = document.createElement('li');
             let check = document.createElement('input');
-            let altDel = document.createElement('i');
-            altDel.className = 'far fa-trash-alt';
             task.appendChild(check).setAttribute("type", "checkbox");
             check.className = 'check-task-button';
             check.checked = todoList.taskList[i].done;
             task.id = i;
-            check.id = i;
             task.insertAdjacentText('beforeend', todoList.taskList[i].text);
             task.appendChild(this.createDeleteButton());
-            task.appendChild(altDel);
             ul.appendChild(task);
         };
 
     },
 
     createDeleteButton: function() {
-        var deleteButton = document.createElement('Button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.className = 'deleteButton';
+        var deleteButton = document.createElement('i');
+        deleteButton.className = 'far fa-trash-alt';
+        deleteButton.setAttribute('data-selector', 'delete-button');
         return deleteButton;
 
     },
@@ -72,27 +68,27 @@ var view = {
         todosUl.addEventListener('click', function(event) {
             // Get the element thas was clicked on
             var elementClicked = event.target;
-
+    
             //Check on class names
             switch (elementClicked.className) {
-                case 'deleteButton':
-                    handlers.delTodo(parseInt(elementClicked.parentNode.id));
-                    break;
-
                 case 'check-task-button' :
                     handlers.toggleCompleted(parseInt(elementClicked.parentNode.id));
-                    break;
-                    
+                    break;  
                 case 'check-all-tasks-button' :
                     handlers.toggleAll();
                     break;
-
                 case 'add-task-button' :
                     handlers.addTodo();
-                    break;
-
-                    
+                    break;       
             }
+            // Check on data-selector values
+            switch (elementClicked.dataset.selector) {
+                case 'delete-button' :
+                    handlers.delTodo(parseInt(elementClicked.parentNode.id));
+                    break;
+            }
+
+            
 
         });
     }
